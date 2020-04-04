c84a883b570570d86453f85220889d0b36a77879
YoelIbarra <yoelibarra98@gmail.com>
2020-04-04 19:45:37 -0300

    CreandoProyecto

---
+++ src/main/java/est/utn/frba/dds/EntregaCero/App.java
@@ -0,0 +1,13 @@
+package est.utn.frba.dds.EntregaCero;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
